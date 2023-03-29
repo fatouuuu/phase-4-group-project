32b049d4c4ec37b8e7107238c8f9b1370a9c3fd4
Bernard <bernard.koech@student.moringaschool.com>
2023-03-29 10:59:51 +0300

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'ingredients/index'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'recipes/create'
+  get 'recipes/update'
+  get 'recipes/destroy'
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
+  get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
