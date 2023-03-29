39b8d291e934fba21a07e208b8506f67ab0df32a
Ayub Ian <iayub.mia@gmail.com>
2023-03-29 20:15:13 +0300

    sessions: Login, signup, current user persistence functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    rescue_from StandardError, with: :standard_error
+
+    # Custom HTTP response
+    def app_response(message: 'Success', status: 200, data: nil) 
+        render json: {message: message,  data: data}, status: status
+    end
+
+    # delete user id in session
+    def remove_user
+        session.delete :user_id
+    end
+
+    # rescue all common errors
+    def standard_error exception
+        app_response(message: 'failed', data: {info: exception.message}, status: :unprocessable_entity)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  # POST /register
   def create
+    user = User.create(user_params)
+    if user.valid?
+      app_response(message: "You've been registered successfully", status: :created, data: user)
+    else
+      app_response(message: "Something went wrong with your registration", status: :unprocessable_entity, data: user.errors.full_messages)
+    end
   end
 
   def update
@@ -9,5 +16,10 @@ class UsersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def user_params
+    params.permit(:username, :email, :bio, :password)
   end
 end
