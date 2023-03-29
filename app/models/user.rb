60554531069b5481c6192c4e570c6f20847d012a
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-29 15:57:03 +0300

    added validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :recipes
+  
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 5 }
+
 end
