75b80c1a6795f181499daff670de4ec6f6364a89
m-njora <mmuthekijora@gmail.com>
2023-04-01 21:51:28 +0300

    done with landing,navbar and footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,59 +1,28 @@
 import React from 'react'
-// import "../Footer.css"
+import { Link } from 'react-router-dom';
+import "../Footer.css"
 
 
 function Footer() {
     return (
-        <footer className="site-footer">
-         <div className="container">
-          <div className="row">
-           <div className="col-sm-12 col-md-6">
-         <h6>Bon Appétit</h6>
-         <p className="text-justify">Id cursus metus aliquam eleifend mi in nulla posuere. Lorem faucibus vitae aliquet</p>
-           </div>
-           <div className="col-xs-6 col-md-3">
-           <h6>About Us</h6>
-           <ul className="footer-links">
-              <li><a href="http://sample.org/about">About Bon Appetite</a></li>
-              <li><a href="http://sample.org/partnerships/">Partnerships</a></li>
-              <li><a href="http://sample.org/customer/">Customer</a></li>
-              <li><a href="http://sample.org//privacy-policy/">Privacy Policy</a></li>
-              <li><a href="http://sample.org/Terms of service">Terms of service</a></li>
-            </ul>
+        <footer >
+        <div className="footer-bottom">
+        <p>copyright &copy;2023 <a href="#">Bon Appétit</a></p>
+        </div>
+        <div className="footer-menu">
+          <ul className="f-menu">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
 
-           </div>
-           <div className="col-xs-6 col-md-3">
-           <h6>Services</h6>
-            <ul className="footer-links">
-              <li><a href="http://sample.org/Menu/">Menu</a></li>
-              <li><a href="http://sample.org/Recipe/">Recipe</a></li>
-              <li><a href="http://sample.org/Delivery/">Delivery</a></li>
-              <li><a href="http://sample.org/Reseller/">Reseller</a></li>
-              <li><a href="http://sample.org/sitemap/">Course</a></li>
-            </ul>
-          </div>
-         </div>
-         </div>
-
-         <div className="col-md-4 col-sm-6 col-xs-12">
-          <h6>Contact Us</h6>
-            <ul className="social-icons">
-              <li><a className="linkedin" href="#"><i className="fa fa-linkedin"></i></a></li>
-              <li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
-              <li><a className="instagram" href="#"><i className="fa fa-instagram"></i></a></li>
-              <li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
-
-            </ul>
-            <div className="col-md-8 col-sm-6 col-xs-12">
-            <p className="copyright-text">
-            Copyright &copy; 2022 ©bonappétit.com 
-            All right reserved.
-            <a href="#">Bon Appetite</a>.
-            </p>
-
-         </div>
-
-          </div>
+          <li>
+            <Link to="/recipes">Recipes</Link>
+          </li>
+          <li>
+            <Link to="/contact">ContactUs</Link>
+          </li>
+          </ul>
+        </div>
 
         </footer>
     )
