d67bcd1dcf9715a9429130fbcc7f95a68cb9ea84
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-04-02 18:05:03 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import RecipeDetail from './components/RecipeDetail';
+import RecipeList from './components/RecipeList';
 
 function App() {
   return (
+ 
     <BrowserRouter>
       <Routes>
         {/* Define your routes here */}
-        
+        <Route path="/recipes" component={RecipeList} />
+        <Route path="/" component={RecipeDetail}/>
         
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App;
+ export default App;
